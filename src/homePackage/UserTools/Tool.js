608389c91d6f0a24838ae12deeae46465c43ed41
bmy3369 <bmy3369@g.rit.edu>
2021-04-02 00:34:14 -0400

    editing of cateogries

--- src/homePackage/UserTools/Tool.js
+++ src/homePackage/UserTools/Tool.js
@@ -3,13 +3,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {Component} from 'react'
 
 import DeleteTool from "./DeleteTool"
+import EditTool from "./EditTool";
 
 class Tool extends Component {
     constructor(props) {
         super(props);
     this.state = {
-        myToolArgs: props.tools
+        myToolArgs: props.tools,
+        categories: [],
+        listCategories: ""
      }
+    }
+
+    displayCategories = () => {
+        let myCategories = this.state.categories[0];
+        for (let i = 1; i < this.state.categories.length; i++) {
+            myCategories += (", " + this.state.categories[i])
+        }
+        this.setState({listCategories: myCategories})
+    }
+
+    updateCategories = (list) => {
+        if (list !== null) {
+            this.setState({categories: list})
+            this.displayCategories()
+        }
+    }
+
+    fetchCategories = () => {
+        fetch('/getToolCategories/' +this.state.myToolArgs[0])
+            .then(
+                response => response.json()
+            ).then(jsonOutput => {
+                this.updateCategories(jsonOutput)
+        })
+    }
+
+    componentDidMount() {
+        this.fetchCategories()
     }
 
     render () {
@@ -18,11 +49,11 @@ class Tool extends Component {
                 <td align={'center'}>{this.state.myToolArgs[0]}</td>
                 <td align={'center'}>{this.state.myToolArgs[1]}</td>
                 <td align={'center'}>{this.state.myToolArgs[2]}</td>
-                <td align={'center'}>n/a</td>
-                <td align={'center'}>n/a</td>
-                <td align={'center'}>n/a</td>
+                <td align={'center'}>{this.state.listCategories}</td>
+                <td align={'center'}>{this.state.myToolArgs[3]}</td>
                 <td align={'center'}>{this.state.myToolArgs[4]}</td>
-                <td align={'center'}>edit</td>
+                <td align={'center'}>{this.state.myToolArgs[5]}</td>
+                <td align={'center'}><EditTool tool={this.state.myToolArgs} categories={this.state.categories}/></td>
                 <td align={'center'}><DeleteTool tool={this.state.myToolArgs[0]}></DeleteTool></td>
             </tr>
         )
