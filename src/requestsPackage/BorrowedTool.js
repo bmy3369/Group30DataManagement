0ffc73da5b4e32ae0c50d3679bdf0db6d1e5e563
Daniel Gardner <drg5567@rit.edu>
2021-04-02 00:39:00 -0400

    implementing overdue dates

--- src/requestsPackage/BorrowedTool.js
+++ src/requestsPackage/BorrowedTool.js
@@ -8,7 +8,9 @@ class BorrowedTool extends Component {
         super(props);
         this.state = {
             myBorrowedToolArgs: props.borrowedTools,
+            color: 'white'
         }
+        this.dateColor()
     }
 
     /**
@@ -37,12 +39,28 @@ class BorrowedTool extends Component {
         this.returnTool()
     }
 
+    dateColor = () => {
+        const lendDate = new Date(this.state.myBorrowedToolArgs[2]);
+        const returnDate = new Date(lendDate.valueOf());
+        //returnDate.setDate(returnDate.getDate() + this.state.myBorrowedToolArgs[3])
+        console.log('lend date: ' + lendDate)
+        //console.log('change in date: ' + returnDate.getDate() + this.state.myBorrowedToolArgs[3])
+        console.log('return date: ' + returnDate.getDate())
+        console.log(new Date() > returnDate)
+        const args = this.state.myBorrowedToolArgs.slice();
+        args[2] = returnDate;
+        this.setState({myBorrowedToolArgs: args})
+        if (new Date() > returnDate) {
+            this.setState({color: 'red'});
+        }
+    }
+
     render() {
         return (
             <tr>
                 <td align={'center'}>{this.state.myBorrowedToolArgs[0]}</td>
                 <td align={'center'}>{this.state.myBorrowedToolArgs[1]}</td>
-                <td align={'center'}>{this.state.myBorrowedToolArgs[2]}</td>
+                <td bgcolor={this.color} align={'center'}>{this.state.myBorrowedToolArgs[2]}</td>
                 <td align={'center'}>{this.state.myBorrowedToolArgs[3]}</td>
                 <td align={'center'}><Button color={'danger'} onClick={this.submitForm}>Return</Button></td>
             </tr>
