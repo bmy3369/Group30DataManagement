07049deb5da5e346bedc44116a19bd6843d06a08
Daniel Gardner <drg5567@rit.edu>
2021-04-23 22:06:25 -0400

    Cleaning up un-needed code copied into Dashboard.js

--- src/homePackage/UserTools/Dashboard.js
+++ src/homePackage/UserTools/Dashboard.js
@@ -37,27 +37,9 @@ class UserTools extends Component {
         this.fetchAllTools()
     }
 
-    displayTool = (tool) => {
-        return (
-            <Tool tools={tool}/>
-        );
-    }
-
-    fetchSearch = (searchtype) => {
-        fetch(searchtype + this.state.currentUser + "/" + this.state.searchParam)
-            .then(
-                response => response.json()
-            ).then(jsonOutput => {
-            console.log(jsonOutput)
-            this.setState({tools: []})
-            this.updateAllTools(jsonOutput)
-        })
-    }
-
     render() {
         return (
             <div className="m-4">
-                <header className="text-center">Tool List</header>
                 <h2 className="text-center">Your Tools Available: </h2>
                 <h2 className="text-center">Lent Tools: </h2>
                 <h2 className="text-center">Borrowed Tools: </h2>
